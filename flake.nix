cc93e692dff6116b7cb632b4877220610fbe69f7
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-10-31 08:40:05 -0500

    update flake to use rust-overlay

--- flake.nix
+++ flake.nix
@@ -3,30 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = inputs:
     with inputs;
     let
+      system = "x86_64-linux";
       pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
+        inherit system;
+        overlays = [
+          rust-overlay.overlays.default
+        ];
       };
     in
     {
       packages.x86_64-linux.default = pkgs.callPackage ./. { };
 
       devShells.x86_64-linux.default = pkgs.mkShell {
-        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+        # RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        # LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
 
         nativeBuildInputs = with pkgs; [
-          cargo
-          rustc
-          rustfmt
-
-          xorg.libxcb
-          xorg.libX11
+          (rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" ];
+          })
         ];
       };
     };
